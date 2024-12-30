689402881878f154026238aabc598cfda28bf9ce
Saltytatertot <saltytatertot55@gmail.com>
2024-12-29 17:11:23 -0700

    Fixed Camera
    Instead of disallowing inheritance of select values, I made the camera nodes top level, thus not inheriting position in the first place, and set the position values to the ship explicitly.

--- Scenes/boat.gd
+++ Scenes/boat.gd
@@ -12,7 +12,6 @@ extends RigidBody3D
 @onready var floaty_contaier = $FloatyContaier.get_children()
 @onready var camera_3d: Camera3D = $Hori/Verti/Camera3D
 
-
 var mouse_movement = Vector2()
 #var current_velocity : Vector3 = (linear_velocity * transform.basis.transposed())
 var moving_rotational_degrees = 0.5
@@ -30,7 +29,13 @@ func _unhandled_input(event: InputEvent) -> void:
 # Central force will propell the whole boat as 1 unit, 
 # apply force will do the individual floaties.
 
-
+#func camera_rotation_gyro() -> void:
+	#var input = hori.rotation_degrees / 360
+	#
+	#if input > 0.5:
+		#input *=-1
+	#var output = input - 2
+	#pass
 
 func floaty_physics() -> void:
 	submerged = false
@@ -41,6 +46,8 @@ func floaty_physics() -> void:
 			#print(floaty)
 			submerged = true
 			apply_force( Vector3.UP * float_force * gravity * depth, floaty.global_position - global_position )
+			#print(float_force, gravity, depth, floaty.global_position - global_position)
+			#print(rotation_degrees.x)
 			#apply_central_force(Vector3.UP * 1 * -floaty.global_transform.origin)
 
 func _integrate_forces(state: PhysicsDirectBodyState3D):
@@ -55,11 +62,38 @@ func _input(event: InputEvent) -> void:
 func _physics_process(_delta: float) -> void:
 	#var floaties = get_tree().get_nodes_in_group("Boat_Nodes")
 	
+	# NOTE: This works, but only for the back of the vehicle.
+	#if hori.rotation_degrees.y > 360 or hori.rotation_degrees.y < -360:
+		#hori.rotation_degrees.y = 0
+	#print(hori.rotation_degrees.y)
+	#if hori.rotation_degrees.y >= -10 and hori.rotation_degrees.y <= 10:
+		#hori.rotation_degrees.z = rotation_degrees.z * -1
+	#elif hori.rotation_degrees.y >= 80 and hori.rotation_degrees.y <= 100:
+		#hori.rotation_degrees.z = rotation_degrees.z * 0.5
+	#elif hori.rotation_degrees.y >= 170 and hori.rotation_degrees.y <= 190:
+		#hori.rotation_degrees.z = rotation_degrees.z
+	#elif hori.rotation_degrees.y >= 260 and hori.rotation_degrees.y <= 280:
+		#hori.rotation_degrees.z = rotation_degrees.z * -0.5
+	hori.global_position = global_position
+	hori.global_position.y = global_position.y + 0
+	hori.global_position.z = global_position.z + 0
+	#hori.global_rotation = global_rotation
+		
+	
+
+	#hori.rotation_degrees.x = rotation_degrees.x * -1
+	#verti.rotation_degrees.y = rotation_degrees.y * -1
+	#hori.RotationEditMode
+	#hori.rotation_degrees.x = -rotation_degrees.x
+	
+	
+	
 	if mouse_movement != Vector2() and Input.get_mouse_mode() == 2: # Checking for MOUSE_MODE_CAPTURED
 #		NOTE 1st person position: X 0.747, Y 5.127, Z -4.435
 #		NOTE 3rd person position: X 0, Y 3.11, Z -5.716
 		hori.rotation_degrees.y -= mouse_movement.x
 		verti.rotation_degrees.x += mouse_movement.y
+		
 		hori.rotation_degrees.z = 0
 		verti.rotation_degrees.z = 0
 		
@@ -72,6 +106,10 @@ func _physics_process(_delta: float) -> void:
 #	Movement is propulsion based, right now it puts the force from the direction of the vector.
 	if Input.is_action_pressed( "Forward" ):
 		apply_central_force( global_transform.basis * Vector3.BACK * 150)
+		var h = hori.rotation_degrees
+		#print("X: ", h.x, " Y: ", h.y, " Z: ", h.z)
+		#print("Y: ", camera_3d.rotation_degrees.y)
+		#print("Z: ", h.z)
 
 	elif Input.is_action_pressed( "Backward" ):
 		apply_central_force( global_transform.basis * Vector3.FORWARD * 100)
--- Scenes/box.gd
+++ Scenes/box.gd
@@ -1,12 +1,10 @@
 extends Node3D
 @onready var water_plane: MeshInstance3D = $"../WaterPlane"
-@export var float_force := 1.00
+@export var float_force := 5.00
 @export var water_drag := 0.05
 @export var water_angular_drag := 0.05
 
 @onready var debrisGroup = get_children()
-
-
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var submerged := false
@@ -14,18 +12,15 @@ var submerged := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for debris in debrisGroup:
-		
-	#var pile = get_tree().get_nodes_in_group("Debris")
-	#for debris in pile:
 		if str(debris).contains("Debris"):
-			debris.mass = 1.25
+			debris.mass = 0.8
 			debris.gravity_scale = 1.0
-			debris.inertia = Vector3(0.1, 0.5, 0.1)
+			debris.inertia = Vector3(0.1, 0.1, 0.1)
 			debris.linear_damp = 1
 			debris.angular_damp = 0.1
 
 #Calculating the debris physics
-func debris_physics(pile) -> void:
+func debris_physics() -> void:
 	for debris in debrisGroup:
 		var depth = water_plane.get_height(debris.global_position) - debris.global_position.y
 		#if str(debris).contains("Debris"):
@@ -43,5 +38,5 @@ func _integrate_forces(state: PhysicsDirectBodyState3D):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	#var pile = get_tree().get_nodes_in_group("Debris")
-	var pile = get_tree().get_nodes_in_group("Debris")
-	debris_physics(pile)
+	#var pile = get_tree().get_nodes_in_group("Debris")
+	debris_physics()
